6c6975757044d6c5a6b9b7f7c41da10fbe3ef2a1
qarq90 <scythethroughtheheart05@gmail.com>
2024-08-04 18:51:08 +0100

    Project 1.0 - 04/08/2024

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,7 @@ import {Button} from "@/components/ui/Button";
 import {useEffect, useState} from "react";
 import {ProfileEdit} from "@/components/ProfileEdit";
 import {useRouter} from "next/navigation";
-import {AnimatePresence} from "framer-motion";
+import {AnimatePresence, motion} from "framer-motion";
 import {UserIcon} from "../../../public/icons/UserIcon";
 import {useFetchUser} from "@/hooks/useFetchUser";
 import CollectionsStore from "@/stores/CollectionsStore";
@@ -15,6 +15,10 @@ import VideoStore from "@/stores/VideoStore";
 import {CollectionsGrid} from "@/components/CollectionsGrid";
 import g from "@/styles/globals.module.css";
 import {useCollectionsFetcher} from "@/hooks/useCollectionsFetcher";
+import {SkeletonAlpha} from "@/components/SkeletonAlpha";
+import {containerVariants} from "@/styles/animations/scale";
+import {EmptySpace} from "@/components/ui/EmptySpace";
+import {FaImages, FaVideo} from "react-icons/fa";
 
 export default function Page() {
 
@@ -25,6 +29,8 @@ export default function Page() {
 	const {fetchUserAlbums, fetchUserTapes} = useCollectionsFetcher();
 
 	const {userId} = UserStore()
+
+	const [loading,setLoading] = useState(true)
 
 	const {
 		albumsStore,
@@ -80,6 +86,12 @@ export default function Page() {
 				fetchUserTapes(userId).then(() => null);
 			}
 		})
+
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 1750);
+
+		return () => clearTimeout(timer);
 	}, [fetchUser, fetchUserAlbums, fetchUserTapes, imageFetcher, userId, videoFetcher]);
 
 	return (
@@ -115,29 +127,78 @@ export default function Page() {
 			<AnimatePresence>
 				{editing && <ProfileEdit setEditing={setEditing} editing={editing}/>}
 			</AnimatePresence>
-
 			<div className={g.wrapper}>
-				<CollectionsGrid
-					title="YOUR ALBUMS"
-					collectionStore={albumsStore.map(album => ({
-						id: album.albumId,
-						data: album.albumData,
-						title: album.albumTitle,
-					}))}
-					isFetching={imageFetcher}
-					type="album"
-				/>
-
-				<CollectionsGrid
-					title="YOUR TAPES"
-					collectionStore={tapesStore.map(tape => ({
-						id: tape.tapeId,
-						data: tape.tapeData,
-						title: tape.tapeTitle,
-					}))}
-					isFetching={videoFetcher}
-					type="tape"
-				/>
+				{
+					loading ? (
+						<SkeletonAlpha/>
+					) : (
+						<>
+							{
+								albumsStore.length !== 0 ? (
+									<CollectionsGrid
+										title="YOUR ALBUMS"
+										collectionStore={
+											albumsStore.map(album => (
+												{
+													id: album.albumId,
+													data: album.albumData,
+													title: album.albumTitle,
+												}
+											))
+										}
+										isFetching={imageFetcher}
+										type="album"
+									/>
+								) : (
+									<motion.div
+										variants={containerVariants}
+										initial="hidden"
+										animate="visible"
+									>
+										<span className={g.type}>YOUR ALBUMS</span>
+										<EmptySpace height={"24px"}/>
+										<div className={g.nothing}><FaImages/> No albums found. Save some now to view
+											them
+											later!
+										</div>
+										<EmptySpace height={"48px"}/>
+									</motion.div>
+								)
+							}
+							{
+								tapesStore.length !== 0 ? (
+									<CollectionsGrid
+										title="YOUR TAPES"
+										collectionStore={
+											tapesStore.map(tape => (
+												{
+													id: tape.tapeId,
+													data: tape.tapeData,
+													title: tape.tapeTitle,
+												}
+											))
+										}
+										isFetching={videoFetcher}
+										type="tape"
+									/>
+								) : (
+									<motion.div
+										variants={containerVariants}
+										initial="hidden"
+										animate="visible"
+									>
+										<span className={g.type}>YOUR TAPES</span>
+										<EmptySpace height={"24px"}/>
+										<div className={g.nothing}><FaVideo/> No tapes found. Save some now to view them
+											later!
+										</div>
+										<EmptySpace height={"48px"}/>
+									</motion.div>
+								)
+							}
+						</>
+					)
+				}
 			</div>
 		</>
 	)
